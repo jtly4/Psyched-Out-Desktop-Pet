ac55d443845cf751a6422d8c8052917bc3040e88
Ashley L <luckycrane173@gmail.com>
2025-08-10 18:04:50 -0700

    added zimbo sprite

--- main.gd
+++ main.gd
@@ -3,10 +3,12 @@ extends Node2D
 @onready var character_sprite: Sprite2D = $Pet
 @onready var character_polygon: Polygon2D = $Pet/Polygon2D
 @onready var menu: Control
+@onready var window = preload("res://pet_window.tscn")
 
 func _ready():
-	var size: Vector2i = DisplayServer.screen_get_size()
-	DisplayServer.window_set_size(size)
+	var new_window = window.instantiate()
+	
+	get_tree().root.add_child(new_window)
 	
 	pass
 	
