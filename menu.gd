ac02ddfdbc1170d10837cd899d47939c34498c3a
jtly4 <jtly102@gmail.com>
2025-10-31 21:55:52 -0700

    created menu script

---
+++ menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# Closes the menu window without losing sprite information
+# reference psyched out shop
+func _closeWindow() -> void:
+	pass
+	
+# pick either idle or walking animation
+func _animationType() -> void:
+	pass
